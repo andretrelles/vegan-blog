63655f4fba530d6cc5d6e822ed37748a42b37457
Andre Trelles <andretrelles@hotmail.com>
2015-06-05 13:59:29 -0700

    created a session in the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		
+		user = User.find_by(email: params[:login][:email])
+		if user && user.authenticate(params[:login][:password])
+
+			sessions[:user_id] = user.id.to_s
+			redirect_to users_path
+		else
+			render :new
+		end
 	end
 
 	def destroy
