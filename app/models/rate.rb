948c5fe64f49fd0170657e4cc2106c9049969c6a
Andre Trelles <andretrelles@hotmail.com>
2015-06-16 16:46:23 -0700

    Removed ratyrate_rater from model

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,7 +1,6 @@
 class Rate < ActiveRecord::Base
   belongs_to :rater, :class_name => "Article"
   belongs_to :rateable, :polymorphic => true
-  ratyrate_rater
   #attr_accessible :rate, :dimension
 
 end
