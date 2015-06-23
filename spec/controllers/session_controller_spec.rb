c0219f9b4481ce380fbfebfc8b952df38df157cd
Andre Trelles <andretrelles@hotmail.com>
2015-06-23 12:05:43 -0700

    Adds database to ci build

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe SessionController, type: :controller do
+
+	describe "Session controller" do
+		it "should pass a test" do
+			assert true
+		end
+	end
+
+end
